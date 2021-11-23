b59c4fa0eb052b140725218bb945bd20f8985d92
Aleksandar Jakimovski <a.jakimovski@admin-its-MacBook-Pro.local>
2021-11-23 10:23:28 +0100

    added husky pre-commit hooks

--- pages/api/search.js
+++ pages/api/search.js
@@ -6,7 +6,9 @@ export default function handler(req, res) {
   let posts;
 
   if(process.env.NODE_ENV === "production") {
-    //fetch from cache
+    // Fetch from cache
+    posts = require("../../cache/data").posts;
+
   } else {
     const files = fs.readdirSync(path.join("posts"));
 
